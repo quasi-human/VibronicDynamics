07e68fe4208f6c831dbb0266a9554b568d5576b1
Yuki Matsuda <yuki.matsuda.w@gmail.com>
2018-08-20 23:46:32 +0900

    add constant

---
+++ database/CONSTANT.py
@@ -0,0 +1,7 @@
+HBAR = 1 # (au)
+AU2EV = 27.2113961 # (eV/au)
+SPEED_OF_LIGHT = 2.99792458e8 # (m/s)
+SI2AU_TIME = 4.134137457575e16 # (time in a.u./s)
+SI2AU_ENERGY = 2.2937126583579e17 # (energy in a.u./ J)
+WVN2AU = SI2AU_TIME**(-1) * SPEED_OF_LIGHT * 100 * 2 * np.pi
+KB = SI2AU_ENERGY * 1.380e-23 # Boltzman constant
