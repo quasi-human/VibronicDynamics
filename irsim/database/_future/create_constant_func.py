1b59128c8746c0ab8a816f20d12b31bcda5bbf5c
Yuki Matsuda <yuki.matsuda.w@gmail.com>
2018-08-22 01:18:42 +0900

    mv directories

--- irsim/database/_future/create_constant_func.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-
-def show_sorted_vibronic_energy(VIBRONIC_ENERGY_LEVELS):
-    order = sorted([v for v in VIBRONIC_ENERGY_LEVELS.values()], reverse = False)
-    for v in order:
-        for k, vd in VIBRONIC_ENERGY_LEVELS.items():
-            if v == vd:
-                print(k)
-                print(v)
